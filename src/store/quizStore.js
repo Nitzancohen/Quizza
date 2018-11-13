40eb9962ee69b79b30b67e549941b6ff974eca0b
unknown <danny.michaelov@gmail.com>
2018-11-13 20:51:38 +0200

    started working on store. -dani

---
+++ src/store/quizStore.js
@@ -0,0 +1,16 @@
+import { observable, action } from 'mobx';
+import axios from 'axios';
+
+class QuizStore {
+
+    @observable quizzes = null;
+
+    @action getUser = async (userName) => {
+        const currentUser = await axios.get('http://localhost:8080', {userName});
+        this.quizzes = currentUser.data;
+    }
+
+}
+
+const store = new QuizStore();
+export default store;
