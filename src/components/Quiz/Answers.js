8c000bc4851c23131de325bd87e05fe42e249168
unknown <danny.michaelov@gmail.com>
2018-11-15 11:13:12 +0200

    modified Answers, Quiz components. -dani

--- src/components/Quiz/Answers.js
+++ src/components/Quiz/Answers.js
@@ -1,30 +1,40 @@
 import React, { Component } from "react";
+import { inject } from "mobx-react";
 
+@inject("store")
 class Answers extends Component {
 
     render() {
-        // hardcoded or from DB?
+        const answers = this.props.store.quiz.question[this.props.index].answers
         return (
             <div className="answers">
-                <div>
-                    <input type="radio" name="answer" id="1" value="1" />
-                    <label for="1">1</label>
-                </div>
-                <div>
-                    <input type="radio" name="answer" id="2" value="2" />
-                    <label for="2">2</label>
-                </div>
-                <div>
-                    <input type="radio" name="answer" id="3" value="3" />
-                    <label for="3">3</label>
-                </div>
-                <div>
-                    <input type="radio" name="answer" value="4" />
-                    <label for="4">4</label>
-                </div>
+                {answers.map(answer, i => {
+                    <div className="answer">
+                        <input type="radio" name="answer" id={i} value={i} />
+                        <label for={i}>{i}</label>
+                    </div>
+                })}
             </div>
         )
     }
 }
 
 export default Answers;
+
+
+{/* <div>
+    <input type="radio" name="answer" id="1" value="1" />
+    <label for="1">1</label>
+</div>
+<div>
+    <input type="radio" name="answer" id="2" value="2" />
+    <label for="2">2</label>
+</div>
+<div>
+    <input type="radio" name="answer" id="3" value="3" />
+    <label for="3">3</label>
+</div>
+<div>
+    <input type="radio" name="answer" value="4" />
+    <label for="4">4</label>
+</div> */}
