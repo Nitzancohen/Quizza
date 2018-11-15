328619bda80c80c4d8014a76de91ae6d02204527
אלמוג אוחנה <yuval8698@gmail.com>
2018-11-15 13:26:43 +0200

    question view

--- src/components/Quiz/Questions.js
+++ src/components/Quiz/Questions.js
@@ -1,15 +1,31 @@
 import React, { Component } from "react";
 import Question from './Question';
+import { observer, inject } from "mobx-react";
+import {observable} from 'mobx'
 
+@inject("store")
+@observer
 class Questions extends Component {
+@observable index=0
 
+@action nextquestion(){
+    let length= this.props.store.quiz.questions.length
+if(this.index<length-1){
+    this.index++
+}
+else{
+    //לקרוא לפנוקציה של סיום הקוויז
+}
+}
     render() {
-        let questions= this.props.store.quiz.questions
+        
         return (
             <div className="Questions">
-                {questions.map(  i => {
+            <Question index={this.index} key={this.index} nextquestion={this.nextquestion}/>
+
+                {/* {questions.map(  i => {
                     <Question index={i}  key={i} />
-                })}
+                })} */}
             </div>
         )
     }
