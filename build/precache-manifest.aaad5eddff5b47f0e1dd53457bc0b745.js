1d9c6dc6798ee62d9f029fc38021b14a9d25470a
Nitzancohen <nitzancohen105@gmail.com>
2018-11-21 21:52:57 +0200

    buttons

--- build/precache-manifest.aaad5eddff5b47f0e1dd53457bc0b745.js
+++ build/precache-manifest.aaad5eddff5b47f0e1dd53457bc0b745.js
@@ -4,23 +4,23 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "2363b5cf9813075f277d",
-    "url": "/static/js/main.2363b5cf.chunk.js"
+    "revision": "11d551aee05006568a2a",
+    "url": "/static/js/main.11d551ae.chunk.js"
   },
   {
     "revision": "3da6263a76e493a50cc9",
     "url": "/static/js/1.3da6263a.chunk.js"
   },
   {
-    "revision": "2363b5cf9813075f277d",
-    "url": "/static/css/main.e88198d3.chunk.css"
+    "revision": "11d551aee05006568a2a",
+    "url": "/static/css/main.05b00f44.chunk.css"
   },
   {
     "revision": "3da6263a76e493a50cc9",
     "url": "/static/css/1.9eb0a2d9.chunk.css"
   },
   {
-    "revision": "424b82ce3b57455020e1fe5a2d232631",
+    "revision": "8ff873e27c391793a22a519752b4c7c5",
     "url": "/index.html"
   }
 ];
