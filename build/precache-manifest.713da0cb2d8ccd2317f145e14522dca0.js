896453517c1b0d78658ebf5e981b302ce423466e
Nitzancohen <nitzancohen105@gmail.com>
2018-11-21 15:57:57 +0200

    cloud design

--- build/precache-manifest.713da0cb2d8ccd2317f145e14522dca0.js
+++ build/precache-manifest.713da0cb2d8ccd2317f145e14522dca0.js
@@ -1,30 +1,26 @@
 self.__precacheManifest = [
   {
-    "revision": "d3273e7a67b536557f138004c6328601",
-    "url": "/static/media/Purple-Background-Wallpaper-HD.d3273e7a.jpg"
-  },
-  {
     "revision": "229c360febb4351a89df",
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "69adb120e1ebe8f6ce2f",
-    "url": "/static/js/main.69adb120.chunk.js"
+    "revision": "9da770548b0b8713e4fb",
+    "url": "/static/js/main.9da77054.chunk.js"
   },
   {
     "revision": "3da6263a76e493a50cc9",
     "url": "/static/js/1.3da6263a.chunk.js"
   },
   {
-    "revision": "69adb120e1ebe8f6ce2f",
-    "url": "/static/css/main.42753032.chunk.css"
+    "revision": "9da770548b0b8713e4fb",
+    "url": "/static/css/main.cbb4cfc8.chunk.css"
   },
   {
     "revision": "3da6263a76e493a50cc9",
     "url": "/static/css/1.9eb0a2d9.chunk.css"
   },
   {
-    "revision": "b056adbfb79cb8e767bf98d2d0c24c0b",
+    "revision": "349a8d950130d86403d5d25660898b9c",
     "url": "/index.html"
   }
 ];
