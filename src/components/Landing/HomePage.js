8c5201e77e0732ecce1a1e0bac08a42542da0fd4
Nitzancohen <nitzancohen105@gmail.com>
2018-11-21 18:16:44 +0200

    some css changes

--- src/components/Landing/HomePage.js
+++ src/components/Landing/HomePage.js
@@ -9,19 +9,19 @@ class HomePage extends Component {
             <div className="container">
                 <h1>QUIZZA</h1>
                 <p className="opening">Welcome to Quizza, Are you ready to start the fun? <br />
-                    You have tree options in fornt of you.. you just need to choose one and start playing!!
+                    You have three options in front of you.. you just need to choose one and start playing!!
              </p>
                 <div className="apps">
                     <Link to="/quiz"><div className="app quiz">
                         <h2 className="header">TEST YOURSELF</h2><br />
                         <span className="icon"><i className="fas fa-question"></i></span>
                         <div className="describe">Join us, and get to know yourself better! 
-                            Also you can creat any quiz on your mind!</div>
+                            Also you can create any quiz on your mind!</div>
                     </div></Link>
                     <Link to="/cloud"><div className="app">
                         <h2 className="header">CLOUD GAME</h2><br />
                         <span className="icon"><i className="fas fa-cloud"></i></span>
-                        <div className="describe">Pick your quote for today by our game!</div>
+                        <div className="describe">Pick your quote for today in our game!</div>
                     </div></Link>
                     <Link to="/horoscope"><div className="app horoscope">
                         <h2 className="header">WHAT'S YOUR HOROSCOPE?</h2><br />
