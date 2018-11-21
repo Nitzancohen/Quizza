3bd23953ec9ce15641430b6c325beaee6c21a2c4
Nitzancohen <nitzancohen105@gmail.com>
2018-11-21 14:54:12 +0200

    fixes

---
+++ build/precache-manifest.ac91b4cd3d419640a6932815abc067da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "d3273e7a67b536557f138004c6328601",
+    "url": "/static/media/Purple-Background-Wallpaper-HD.d3273e7a.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "f9aac8a94db1df1e74b4",
+    "url": "/static/js/main.f9aac8a9.chunk.js"
+  },
+  {
+    "revision": "2d15a75a88d4cfe8b3dd",
+    "url": "/static/js/1.2d15a75a.chunk.js"
+  },
+  {
+    "revision": "f9aac8a94db1df1e74b4",
+    "url": "/static/css/main.84622002.chunk.css"
+  },
+  {
+    "revision": "2d15a75a88d4cfe8b3dd",
+    "url": "/static/css/1.414a7095.chunk.css"
+  },
+  {
+    "revision": "d86a73dee9475338ece646c8bcbf4ad0",
+    "url": "/index.html"
+  }
+];
