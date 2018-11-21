6d6fe03fa23bff9e79200ed365fbd92639298b70
Nitzancohen <nitzancohen105@gmail.com>
2018-11-21 23:06:17 +0200

    horoscope

--- build/precache-manifest.c6fb40bfb65d1b10e145c2b098c8064e.js
+++ build/precache-manifest.c6fb40bfb65d1b10e145c2b098c8064e.js
@@ -4,23 +4,23 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "11d551aee05006568a2a",
-    "url": "/static/js/main.11d551ae.chunk.js"
+    "revision": "be20831fe69e31b8e3ac",
+    "url": "/static/js/main.be20831f.chunk.js"
   },
   {
     "revision": "3da6263a76e493a50cc9",
     "url": "/static/js/1.3da6263a.chunk.js"
   },
   {
-    "revision": "11d551aee05006568a2a",
-    "url": "/static/css/main.05b00f44.chunk.css"
+    "revision": "be20831fe69e31b8e3ac",
+    "url": "/static/css/main.21c16955.chunk.css"
   },
   {
     "revision": "3da6263a76e493a50cc9",
     "url": "/static/css/1.9eb0a2d9.chunk.css"
   },
   {
-    "revision": "8ff873e27c391793a22a519752b4c7c5",
+    "revision": "18a8e99d1b207e3b723a4e60a0f09ac4",
     "url": "/index.html"
   }
 ];
